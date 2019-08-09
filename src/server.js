b243a9dcb2b3b22154b2ad11019d4d4bc41146ed
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-09 20:12:49 -0300

    Configurando servidor para ouvir conexões via protocolo websocket

--- src/server.js
+++ src/server.js
@@ -5,16 +5,40 @@ const cors = require('cors');
 const routes = require('./routes');
 
 // Criando instância do express
-const server = express();
+const serverHTTP = express();
+// Extraindo o módulo de HTTP do Node e unindo ao servidor WEBSOCKET
+const serverWEBSOCKET = require('http').Server(serverHTTP);
+const io = require('socket.io')(serverWEBSOCKET);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+	const { user } = socket.handshake.query;
+
+	connectedUsers[user] = socket.id
+});
 
 mongoose.connect('mongodb+srv://tindev:tindev@cluster0-6xi6w.mongodb.net/omnistack8?retryWrites=true&w=majority', { 
 	useNewUrlParser: true 
 });
 
-server.use(cors());
+/** 
+ * Middleware utilizado para interceptar as requisições, 
+ * salvar instância do socket e os usuários conectados
+ * e repassar para os controllers
+ */
+serverHTTP.use((request, response, next) => {
+	request.io = io;
+	request.connectedUsers = connectedUsers;
+
+	// Segue o fluxo das rotas normalmente
+	return next();
+});
+
+serverHTTP.use(cors());
 // Habilitando a utilização de json dentro do express
-server.use(express.json());
+serverHTTP.use(express.json());
 // Use permite importar as configurações de outros arquivos/módulos
-server.use(routes);
-// Ouvindo a porta 3333
-server.listen(3333);
+serverHTTP.use(routes);
+// Ouvindo a porta 3333 e habilitando servidor para atender a requisições HTTP e WEBSOCKET
+serverWEBSOCKET.listen(3333);
